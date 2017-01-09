fc5d94da7d519c9f9957000d4c9e42a2e0afb636
Matthew <mattheworr1@hotmail.co.uk>
2017-01-09 19:09:10 +0000

    Types

---
+++ src/app/types.ts
@@ -0,0 +1,42 @@
+class Types {
+
+    private normal: string;
+    private fighting: string;
+    private flying: string;
+    private poison: string;
+    private ground: string;
+    private rock: string;
+    private bug: string;
+    private ghost: string;
+    private steel: string;
+    private fire: string;
+    private water: string;
+    private grass: string;
+    private electric: string;
+    private psychic: string;
+    private ice: string;
+    private dragon: string;
+    private dark: string;
+    private fairy: string;
+
+    constructor (normal: string, fighting: string, flying: string, poison: string, ground: string, rock: string, bug: string, ghost: string, steel: string, fire: string, water: string, grass: string, electric: string, psychic: string, ice: string, dragon: string, dark: string, fairy: string) {
+        this.normal = normal;
+        this.fighting = fighting;
+        this.flying = flying;
+        this.poison = poison;
+        this.ground = ground;
+        this.rock = rock;
+        this.bug = bug;
+        this.ghost = ghost;
+        this.steel = steel;
+        this.fire = fire;
+        this.water = water;
+        this.grass = grass;
+        this.electric = electric;
+        this.psychic = psychic;
+        this.ice = ice;
+        this.dragon = dragon;
+        this.dark = dark;
+        this.fairy = fairy;
+    }
+}
