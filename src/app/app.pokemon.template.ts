5b58883cd85409ea9a6774f811cfce6fb45327b2
Matthew <mattheworr1@hotmail.co.uk>
2017-01-09 17:05:31 +0000

    template

---
+++ src/app/app.pokemon.template.ts
@@ -0,0 +1,25 @@
+class PokemonTemplate {
+
+    pokeName: string;
+    pokeType1: string;
+    pokeType2: string;
+    pokeEvo1: string;
+    pokeEvo2: string;
+    pokeCatch: string;
+    pokeLevel: string;
+
+    constructor(name1: string, type1: string, type2: string, evo1: string, evo2: string, catch1: string, level1: string) {
+        this.pokeName = name1;
+        this.pokeType1 = type1;
+        this.pokeType2 = type2;
+        this.pokeEvo1 = evo1;
+        this.pokeEvo2 = evo2;
+        this.pokeCatch = catch1;
+        this.pokeLevel = level1;
+
+    }
+
+    pokemon() {
+
+    }
+}
