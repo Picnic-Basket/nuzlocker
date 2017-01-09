4c699b9213d583ca567e474c3b557a0bd7356051
Matthew <mattheworr1@hotmail.co.uk>
2017-01-09 17:31:47 +0000

    template

--- src/app/app.pokemon.template.ts
+++ src/app/app.pokemon.template.ts
@@ -1,6 +1,7 @@
 class Pokemon {
 
     pokeName: string;
+    pokeImage: string;
     pokeType1: string;
     pokeType2: string;
     pokeEvo1: string;
@@ -8,7 +9,7 @@ class Pokemon {
     pokeCatch: string;
     pokeLevel: string;
 
-    constructor(name1: string, type1: string, type2: string, evo1: string, evo2: string, catch1: string, level1: string) {
+    constructor(name1: string, image1: string, type1: string, type2: string, evo1: string, evo2: string, catch1: string, level1: string) {
         this.pokeName = name1;
         this.pokeType1 = type1;
         this.pokeType2 = type2;
@@ -20,4 +21,4 @@ class Pokemon {
     }
 }
 
-    let pidgey = new Pokemon("Pidgey", "Normal", "Flying", "Pidgeotto", "Pidgeot", "255", "medium slow");
+    let pidgey = new Pokemon("Pidgey", "nuzlocker\\src\\assets\\pokegen1\\016Pidgey.png", "Normal", "Flying", "Pidgeotto", "Pidgeot", "255", "medium slow");
